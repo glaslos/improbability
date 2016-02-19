c544bff47a2ad9c770ab0cc157630241823ee4c0
glaslos <lukas.rist@bluecoat.com>
2016-02-19 16:53:03 +0100

    added support for scalable bloom filter

--- server.go
+++ server.go
@@ -13,6 +13,7 @@ import (
   "github.com/AndreasBriese/bbloom"
   "github.com/zhenjl/bloom"
   "github.com/zhenjl/bloom/partitioned"
+  "github.com/zhenjl/bloom/scalable"
   "github.com/spaolacci/murmur3"
 )
 
@@ -21,6 +22,7 @@ var bfilters = make(map[string]*bloomd.Filter)
 var cbfilters = make(map[string]*cbfilter.Filter)
 var bbfilters = make(map[string]bbloom.Bloom)
 var pbfilters = make(map[string]bloom.Bloom)
+var sbfilters = make(map[string]bloom.Bloom)
 
 var mutex = &sync.Mutex{}
 
@@ -39,6 +41,7 @@ func main() {
   router.HandleFunc("/api/cbfilter/{name:[a-z]+}", CBFilter)
   router.HandleFunc("/api/bbfilter/{name:[a-z]+}", BBFilter)
   router.HandleFunc("/api/pbfilter/{name:[a-z]+}", PBFilter)
+  router.HandleFunc("/api/sbfilter/{name:[a-z]+}", SBFilter)
   log.Fatal(http.ListenAndServe(":8080", router))
 }
 
@@ -162,6 +165,27 @@ func PBFilter(w http.ResponseWriter, r *http.Request) {
     bf := partitioned.New(100000)
     bf.SetHasher(murmur3.New64())
     pbfilters[name] = bf
+    resp.Result = true
+  } else if r.Method == "POST" {
+    bf.Add(item)
+    resp.Result = true
+  } else if r.Method == "GET" {
+    resp.Result = bf.Check(item)
+  }
+  json.NewEncoder(w).Encode(resp)
+}
+
+func SBFilter(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  name := vars["name"]
+  item := []byte(r.URL.Query().Get("item"))
+  resp := Response{Item: r.URL.Query().Get("item"), Method: r.Method, Endpoint: r.URL.Path}
+  bf := sbfilters[name]
+  if r.Method == "PUT" {
+    bf := scalable.New(100000)
+    bf.SetHasher(murmur3.New64())
+    bf.(*scalable.ScalableBloom).SetBloomFilter(partitioned.New)
+    sbfilters[name] = bf
     resp.Result = true
   } else if r.Method == "POST" {
     bf.Add(item)
