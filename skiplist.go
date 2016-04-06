d90e25d4ce5388e9a945d4baa5235efa4c843bc5
glaslos <lukas.rist@bluecoat.com>
2016-04-06 12:14:57 +0200

    fixing skiplist ret value

--- skiplist.go
+++ skiplist.go
@@ -20,29 +20,28 @@ func SkipList(w http.ResponseWriter, r *http.Request) {
   name := vars["name"]
   item := r.URL.Query().Get("item")
   resp := SLResponse{Response: Response{Item: item, Method: r.Method, Endpoint: r.URL.Path}}
-  slist := slists[name]
   if r.Method == "PUT" {
     slist := skiplist.NewIntMap()
     slists[name] = slist
     resp.Result = true
-  } else if r.Method == "POST" {
-    key := slist.Len()
-    key ++
-    slist.Set(key, item)
-    resp.Result = true
-  } else if r.Method == "GET" {
-    i, _ := strconv.Atoi(item)
-    str, ok := slist.Get(i)
-    if ok {
-      if test_string, ok := str.(string); ok {
-        resp.Value = test_string
-        resp.Result = true
-      } else {
-        resp.Result = false
-      }
-      resp.Result = true
+  } else {
+    slist, ok := slists[name]
+    if ok != true {
+      http.Error(w, "store not found", http.StatusNotFound)
+      return
     } else {
-      resp.Result = false
+      if r.Method == "POST" {
+        key := slist.Len()
+        key ++
+        slist.Set(key, item)
+        resp.Result = true
+        resp.Value = strconv.Itoa(key)
+      } else if r.Method == "GET" {
+        i, _ := strconv.Atoi(item)
+        val, ok := slist.Get(i)
+        resp.Result = ok
+        resp.Value, _ = val.(string)
+      }
     }
   }
   json.NewEncoder(w).Encode(resp)
