35bfdc8312159f7ebf671b81d96cdf74e58df559
glaslos <lukas.rist@bluecoat.com>
2016-04-11 17:07:08 +0200

    support for posting large data sets

--- topk.go
+++ topk.go
@@ -1,35 +1,48 @@
 package main
 
 import (
-  gotopk "github.com/dgryski/go-topk"
-  "github.com/gorilla/mux"
-  "net/http"
-  "encoding/json"
+	"encoding/json"
+	"net/http"
+
+	gotopk "github.com/dgryski/go-topk"
+	"github.com/gorilla/mux"
 )
 
-type TKResponse struct {
-  Response
-  Value     []gotopk.Element  `json:"value"`
+type tkResponse struct {
+	Response
+	Value []gotopk.Element `json:"value"`
 }
 
 var topks = make(map[string]*gotopk.Stream)
 
-func TopK(w http.ResponseWriter, r *http.Request) {
-  vars := mux.Vars(r)
-  name := vars["name"]
-  item := r.URL.Query().Get("item")
-  resp := TKResponse{Response: Response{Item: item, Method: r.Method, Endpoint: r.URL.Path}}
-  topk := topks[name]
-  if r.Method == "PUT" {
-    topk := gotopk.New(100)
-    topks[name] = topk
-    resp.Result = true
-  } else if r.Method == "POST" {
-    topk.Insert(item, 1)
-    resp.Result = true
-  } else if r.Method == "GET" {
-    resp.Value = topk.Keys()
-    resp.Result = true
-  }
-  json.NewEncoder(w).Encode(resp)
+func topK(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["name"]
+	item := r.URL.Query().Get("item")
+	resp := tkResponse{Response: Response{Item: item, Method: r.Method, Endpoint: r.URL.Path}}
+	topk := topks[name]
+	if r.Method == "PUT" {
+		topk := gotopk.New(100)
+		topks[name] = topk
+		resp.Result = true
+	} else if r.Method == "POST" {
+		if len(item) != 0 {
+			topk.Insert(item, 1)
+		} else {
+			decoder := json.NewDecoder(r.Body)
+			var hashes map[string]interface{}
+			err := decoder.Decode(&hashes)
+			if err != nil {
+				panic(err)
+			}
+			for hash := range hashes {
+				topk.Insert(hash, 1)
+			}
+		}
+		resp.Result = true
+	} else if r.Method == "GET" {
+		resp.Value = topk.Keys()
+		resp.Result = true
+	}
+	json.NewEncoder(w).Encode(resp)
 }
