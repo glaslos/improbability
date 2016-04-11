26cfc623e13625b35ad5fc12111c6fc6619c404a
glaslos <lukas.rist@bluecoat.com>
2016-04-11 17:08:55 +0200

    minor cleanup

--- routes.go
+++ routes.go
@@ -1,31 +1,33 @@
 package main
 
 import (
-  "net/http"
-  "encoding/json"
-  "github.com/gorilla/mux"
-  )
+	"encoding/json"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
 
+// Index handles requests to the api index endpoint
 func Index(w http.ResponseWriter, r *http.Request) {
-  resp := Response{Method: r.Method, Endpoint: r.URL.Path}
-  json.NewEncoder(w).Encode(resp)
+	resp := Response{Method: r.Method, Endpoint: r.URL.Path}
+	json.NewEncoder(w).Encode(resp)
 }
 
+// GetRouter returns a router for the http server
 func GetRouter() *mux.Router {
-  router := mux.NewRouter().StrictSlash(true)
-  router.HandleFunc("/api/", Index)
-  router.HandleFunc("/api/cuckoofilter/{name:[a-z]+}", CuckooFilter)
-  router.HandleFunc("/api/bloomd/{name:[a-z]+}", BloomdFilter)
-  router.HandleFunc("/api/cbfilter/{name:[a-z]+}", CBFilter)
-  router.HandleFunc("/api/bbfilter/{name:[a-z]+}", BBFilter)
-  router.HandleFunc("/api/pbfilter/{name:[a-z]+}", PBFilter)
-  router.HandleFunc("/api/sbfilter/{name:[a-z]+}", SBFilter)
-  router.HandleFunc("/api/hllplus/{name:[a-z]+}", HLLPlus)
-  router.HandleFunc("/api/skiplist/{name:[a-z]+}", SkipList)
-  router.HandleFunc("/api/treap/{name:[a-z]+}", Treap)
-  router.HandleFunc("/api/cml/{name:[a-z]+}", CountMinLog)
-  router.HandleFunc("/api/cms/{name:[a-z]+}", CountMinSketch)
-  router.HandleFunc("/api/topk/{name:[a-z]+}", TopK)
-  return router
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/api/", Index)
+	router.HandleFunc("/api/cuckoofilter/{name:[a-z]+}", CuckooFilter)
+	router.HandleFunc("/api/bloomd/{name:[a-z]+}", BloomdFilter)
+	router.HandleFunc("/api/cbfilter/{name:[a-z]+}", CBFilter)
+	router.HandleFunc("/api/bbfilter/{name:[a-z]+}", BBFilter)
+	router.HandleFunc("/api/pbfilter/{name:[a-z]+}", PBFilter)
+	router.HandleFunc("/api/sbfilter/{name:[a-z]+}", SBFilter)
+	router.HandleFunc("/api/hllplus/{name:[a-z]+}", HLLPlus)
+	router.HandleFunc("/api/skiplist/{name:[a-z]+}", SkipList)
+	router.HandleFunc("/api/treap/{name:[a-z]+}", Treap)
+	router.HandleFunc("/api/cml/{name:[a-z]+}", CountMinLog)
+	router.HandleFunc("/api/cms/{name:[a-z]+}", CountMinSketch)
+	router.HandleFunc("/api/topk/{name:[a-z]+}", topK)
+	return router
 }
--- server.go
+++ server.go
@@ -1,33 +1,35 @@
 package main
 
 import (
-  "log"
-  "net/http"
+	"log"
+	"net/http"
 
-  "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
+// Response default struct
 type Response struct {
-  Item      string  `json:"item"`
-  Result    bool    `json:"result"`
-  Method    string  `json:"method"`
-  Endpoint  string  `json:"endpoint"`
+	Item     string `json:"item"`
+	Result   bool   `json:"result"`
+	Method   string `json:"method"`
+	Endpoint string `json:"endpoint"`
 }
 
+// Middleware for auth check
 func Middleware(router *mux.Router) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        //log.Println("middleware", r.URL)
-        auth_key := r.URL.Query().Get("auth")
-        if len(auth_key) == 0 {
-          http.Error(w, "missing auth", http.StatusUnauthorized)
-          return
-        }
-        //log.Println(auth_key)
-        router.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("middleware", r.Method, r.URL)
+		authKey := r.URL.Query().Get("auth")
+		if authKey != "changeme" {
+			http.Error(w, "invalid auth", http.StatusUnauthorized)
+			return
+		}
+		//log.Println(auth_key)
+		router.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-  router := GetRouter()
-  log.Fatal(http.ListenAndServe(":8080", Middleware(router)))
+	router := GetRouter()
+	log.Fatal(http.ListenAndServe(":8008", Middleware(router)))
 }
